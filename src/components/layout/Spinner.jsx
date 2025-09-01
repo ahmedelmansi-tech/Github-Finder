ba37b3918d6021ff0c5c4d994d359f5ef5dd3e88
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2025-09-01 15:30:04 +0300

    spinner width

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -3,7 +3,12 @@ import loadingSpinner from "./assets/spinner.gif";
 const Spinner = () => {
   return (
     <div className="flex justify-center">
-      <img width={200} src={loadingSpinner} alt="Loading..." />
+      <img
+        width={200}
+        src={loadingSpinner}
+        alt="Loading..."
+        className="sm:w150"
+      />
     </div>
   );
 };
